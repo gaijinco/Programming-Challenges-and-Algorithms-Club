b4db2aa554674da731162810bf30ecccc78dbcdf
dcifuen <dcifuen@gmail.com>
2014-08-07 15:16:14 -0500

    Accepted solutions for #612 and #673

--- 612 DNA Sorting/dcifuen.java
+++ 612 DNA Sorting/dcifuen.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		new Main().sortDNASequences();
+		new dcifuen().sortDNASequences();
 	}
 
 	void sortDNASequences() throws Exception {
---
+++ 673 Parentheses Balance/dcifuen.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// Scanner sc = new Scanner(new File("test.txt"));
+		Scanner sc = new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int totalStrings = Integer.parseInt(sc.nextLine());
+		for (int i = 0; i < totalStrings; i++) {
+			char[] line = sc.nextLine().toCharArray();
+			boolean isCorrect = true;
+			// Counter for the number of open "("
+			Deque<Character> stack = new LinkedList<>();
+			for (int j = 0; j < line.length && isCorrect; j++) {
+				if (line[j] == '(' || line[j] == '[') {
+					stack.push(line[j]);
+				} else {
+					if (stack.isEmpty()) {
+						isCorrect = false;
+					} else {
+						char lastChar = stack.removeFirst();
+						if ((line[j] == ')' && lastChar != '(')
+								|| (line[j] == ']' && lastChar != '[')) {
+							isCorrect = false;
+						}
+					}
+
+				}
+			}
+			out.println(isCorrect && stack.isEmpty() ? "Yes" : "No");
+		}
+		// Close resources
+		sc.close();
+		out.close();
+	}
+}
